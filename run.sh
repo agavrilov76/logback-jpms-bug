a5e0b1803bae4e4075662a71ecb36ce50e9272c6
Alexey Gavrilov <agavrilov@telenordigital.com>
2022-09-19 15:42:35 +0200

    Update run.sh script

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@ set -e
 
 mvn clean compile
 echo "###### Running without modules"
-echo "###### Prints INFO only as expected"
+echo "###### Prints OK: INFO"
 
 java -cp target/classes:"$(cat target/classpath.txt)" com.github.agavrilov.logback.Main
 
 echo "###### Running with modules"
-echo "###### !!! Prints both INFO and DEBUG ignoring the logback.xml config"
+echo "###### !!! Error"
 
 java -p target/classes:"$(cat target/classpath.txt)" -m com.github.agavrilov.logback/com.github.agavrilov.logback.Main
